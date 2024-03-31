326bf4dbd2271dd3c9024e42fe9a37fce35d8a60
Toan Phung <toan.phung@proteo.info>
2024-03-31 16:06:55 +0100

    update

--- src/app/protocol-session/media-presenter/media-presenter.component.ts
+++ src/app/protocol-session/media-presenter/media-presenter.component.ts
@@ -1,11 +1,15 @@
 import {Component, Input} from '@angular/core';
 import {Annotation} from "../../annotation";
 import {WebService} from "../../web.service";
+import {parse} from "@plussub/srt-vtt-parser";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-media-presenter',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './media-presenter.component.html',
   styleUrl: './media-presenter.component.scss'
 })
@@ -13,6 +17,8 @@ export class MediaPresenterComponent {
   _annotation?: Annotation;
   mediaURL: string = ''
   transcription: string = ''
+  subtitles: {entries: {from: number, id: string, text: string, to: number}[]} = {entries: []}
+  currentSubtitleID: number = -1
   @Input() set annotation(value: Annotation) {
     this._annotation = value
     //this.web.getAnnotationImageBlobUrl(value.id).subscribe((url) => {
@@ -23,6 +29,8 @@ export class MediaPresenterComponent {
       // convert value.transcription text to ObjectURL
       const blob = new Blob([value.transcription], {type: 'text/plain'})
       this.transcription = URL.createObjectURL(blob)
+      this.subtitles = parse(value.transcription)
+      console.log(this.subtitles)
     }
     this.web.getSignedURL(value.id).subscribe((token: any) => {
       this.mediaURL = `${this.web.baseURL}/api/annotation/download_signed/?token=${token["signed_token"]}`
@@ -34,4 +42,40 @@ export class MediaPresenterComponent {
   }
   constructor(private web: WebService) { }
 
+  msToTime(duration: number) {
+    const milliseconds = Math.floor((duration % 1000) / 100)
+    let seconds = Math.floor((duration / 1000) % 60)
+    let minutes = Math.floor((duration / (1000 * 60)) % 60)
+    let hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+    hours = (hours < 10) ? 0 + hours : hours
+    minutes = (minutes < 10) ? 0 + minutes : minutes
+    seconds = (seconds < 10) ? 0 + seconds : seconds
+    let strHours = hours.toString()
+    if (strHours.length === 1) {
+      strHours = "0" + strHours
+    }
+    let strMinutes = minutes.toString()
+    if (strMinutes.length === 1) {
+      strMinutes = "0"+strMinutes
+    }
+    let strSeconds = seconds.toString()
+
+    if (strSeconds.length === 1) {
+      strSeconds = "0"+strSeconds
+    }
+    return strHours + ":" + strMinutes + ":" + strSeconds + "." + milliseconds
+  }
+
+  highLightSubtitle(event: any) {
+    const currentTime = event.target.currentTime * 1000
+    const currentSubtitle = this.subtitles.entries.findIndex((entry) => {
+      return currentTime >= entry.from && currentTime <= entry.to
+    })
+    console.log(currentSubtitle)
+    if (currentSubtitle > -1) {
+      this.currentSubtitleID = currentSubtitle
+    }
+
+  }
 }
