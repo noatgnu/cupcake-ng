768eb0ee83ad65869bf0be7de43f7a7a00986066
Toan Phung <toan.phung@proteo.info>
2024-04-24 13:38:49 +0100

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,6 +16,8 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withInterceptors([authInterceptor])
     ),
-    importProvidersFrom(QuillModule.forRoot())
+    importProvidersFrom(QuillModule.forRoot(
+
+    ))
   ],
 };
--- src/app/protocol-editor/protocol-editor.component.ts
+++ src/app/protocol-editor/protocol-editor.component.ts
@@ -35,6 +35,27 @@ export class ProtocolEditorComponent {
     currentSection: new FormControl<number>(0)
   })
 
+  editorConfig = {
+    toolbar: {
+      container: [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'align': [] }],
+
+        ['clean'],                                         // remove formatting button
+
+        ['link', 'image', 'video']                         // link and image, video
+      ]
+    }
+  }
+
   steps: FormGroup[] = []
   sections: {data: ProtocolSection, steps: ProtocolStep[], currentStep: number}[] = []
   protocol?: Protocol
