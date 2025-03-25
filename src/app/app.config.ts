8c0e51167e96309a0e93213f8d1ac3ca8e2707b2
Toan Phung <toan.phung@proteo.info>
2025-03-25 21:39:12 +0000

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,49 +1,17 @@
-import {APP_INITIALIZER, ApplicationConfig, importProvidersFrom, inject, provideAppInitializer} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, inject, provideAppInitializer} from '@angular/core';
 import {
-  PreloadAllModules,
   provideRouter,
   withComponentInputBinding,
   withHashLocation,
-  withPreloading
 } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import {authInterceptor} from "./auth.interceptor";
 import {QuillModule} from "ngx-quill";
-import {LoadingTrackerService} from "./loading-tracker.service";
 import {DOCUMENT} from "@angular/common";
 
-export function initializeAppFactory(loadingTracker: LoadingTrackerService) {
-  return () => {
-    const originalAppendChild = document.head.appendChild.bind(document.head);
-    // @ts-ignore
-    document.head.appendChild = (element: HTMLElement) => {
-      console.log(element);
-      if (element.tagName === 'SCRIPT' && (element as HTMLScriptElement).src) {
-        const scriptElement = element as HTMLScriptElement;
-        const chunkName = scriptElement.src.split('/').pop();
-        loadingTracker.setLoadingChunk(chunkName || 'unknown');
-        scriptElement.onload = () => loadingTracker.clearLoadingChunk();
-      }
-      return originalAppendChild(element);
-    };
-    document.addEventListener('DOMContentLoaded', () => {
-      const originalAppendChildBody = document.body.appendChild.bind(document.body);
-      // @ts-ignore
-      document.body.appendChild = (element: HTMLElement) => {
-        console.log(element);
-        if ((element.tagName === 'SCRIPT' || element.tagName === 'LINK') && (element as HTMLScriptElement).src) {
-          const scriptElement = element as HTMLScriptElement;
-          const chunkName = scriptElement.src.split('/').pop();
-          loadingTracker.setLoadingChunk(chunkName || 'unknown');
-          scriptElement.onload = () => loadingTracker.clearLoadingChunk();
-        }
-        return originalAppendChildBody(element);
-      };
-    });
-  };
-}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -57,13 +25,7 @@ export const appConfig: ApplicationConfig = {
       if (window) {
         window['_appStarted'] = true;
       }
-    }),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeAppFactory,
-      deps: [LoadingTrackerService],
-      multi: true
-    }
+    })
     ,
     provideHttpClient(
       withInterceptors([authInterceptor])
