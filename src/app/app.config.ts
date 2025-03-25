00e34f38d606d38e5ba6f6f2a5716839cf2d6da8
Toan Phung <toan.phung@proteo.info>
2025-03-25 20:10:38 +0000

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -50,7 +50,6 @@ export const appConfig: ApplicationConfig = {
       routes,
       withHashLocation(),
       withComponentInputBinding(),
-      withPreloading(PreloadAllModules)
     ),
     {
       provide: APP_INITIALIZER,
